81cd977b859e778ce0d947af675f97c912300a7c
Rorisang Putu <rorisangputu@gmail.com>
2025-06-22 11:59:04 +0200

    importing actions

---
+++ src/app/admin/products/_components/ProductsActions.tsx
@@ -0,0 +1,50 @@
+import { DropdownMenuItem } from "@/components/ui/dropdown-menu";
+import { useTransition } from "react";
+import {
+  deleteProduct,
+  toggleProductAvailability,
+} from "../../_actions/actions";
+
+export function ActiveToggleDropdownItem({
+  id,
+  isAvailableForPurchase,
+}: {
+  id: string;
+  isAvailableForPurchase: boolean;
+}) {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <DropdownMenuItem
+      disabled={isPending}
+      onClick={() => {
+        startTransition(async () => {
+          await toggleProductAvailability(id, !isAvailableForPurchase);
+        });
+      }}
+    >
+      {isAvailableForPurchase ? "Deactivate" : "Activate"}
+    </DropdownMenuItem>
+  );
+}
+
+export function DeleteDropdownItem({
+  id,
+  disabled,
+}: {
+  id: string;
+  disabled: boolean;
+}) {
+  const [isPending, startTransition] = useTransition();
+  return (
+    <DropdownMenuItem
+      disabled={disabled || isPending}
+      onClick={() => {
+        startTransition(async () => {
+          await deleteProduct(id);
+        });
+      }}
+    >
+      Delete
+    </DropdownMenuItem>
+  );
+}
