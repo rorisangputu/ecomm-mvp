3f5bc56b988391a7e20e87f74937212d9df910a1
Rorisang Putu <rorisangputu@gmail.com>
2025-06-22 11:58:39 +0200

    creating delete product action

--- src/app/admin/_actions/actions.ts
+++ src/app/admin/_actions/actions.ts
@@ -4,7 +4,7 @@
 import db from "@/db/db";
 import { z } from "zod";
 import fs from "fs/promises";
-import { redirect } from "next/navigation";
+import { notFound, redirect } from "next/navigation";
 
 // Define the schema for the form data
 const fileSchema = z.instanceof(File, { message: "Required" });
@@ -77,4 +77,9 @@ export async function toggleProductAvailability(
     where: { id },
     data: { isAvailableForPurchase },
   });
+}
+
+export async function deleteProduct(id: string) {
+  const product = await db.product.delete({ where: { id } });
+  if (product === null) return notFound();
 }
