55ec800286f93692e8eda7f34a57d5f51bd2221a
Rorisang Putu <rorisangputu@gmail.com>
2025-06-23 21:00:16 +0200

    commit

--- src/app/(customerFacing)/products/page.tsx
+++ src/app/(customerFacing)/products/page.tsx
@@ -1,7 +1,38 @@
-export default async function ProductPage() {
+import { ProductCard, ProductCardSkeleton } from "@/components/ProductCard";
+import db from "@/db/db";
+import { Suspense } from "react";
+
+async function getProducts() {
+  return await db.product.findMany({
+    where: { isAvailableForPurchase: true },
+    orderBy: { name: "asc" },
+  });
+}
+
+export default async function ProductsPage() {
   return (
-    <div>
-      <h1>Hellllooooo</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <Suspense
+        fallback={
+          <>
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+            <ProductCardSkeleton />
+          </>
+        }
+      >
+        <ProductsSuspense />
+      </Suspense>
     </div>
   );
+}
+
+async function ProductsSuspense() {
+  const products = await getProducts();
+  return products.map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
 }
