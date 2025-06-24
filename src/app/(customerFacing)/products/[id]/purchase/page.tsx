6ae9c7d57d9e2bf31464563bd39738c27ad9685c
Rorisang Putu <rorisangputu@gmail.com>
2025-06-24 13:49:01 +0200

    creating stripe payment intent

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,8 +1,9 @@
 import db from "@/db/db";
 import { notFound } from "next/navigation";
 import Stripe from "stripe";
+import CheckoutForm from "./_components/CheckoutForm";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 export default async function PurchasePage({
   params,
 }: {
@@ -14,9 +15,16 @@ export default async function PurchasePage({
   });
 
   if (product == null) return notFound();
-  return (
-    <div>
-      <h1>Hii {id}</h1>
-    </div>
-  );
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "ZAR",
+    metadata: { productId: product.id },
+  });
+
+  if (paymentIntent.client_secret == null) {
+    throw Error("Stripe failed to create payment intent");
+  }
+
+    return <CheckoutForm product={product} clientSecret={paymentIntent.client_secret} />;
 }
