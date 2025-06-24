0c2046b736ab2550ad906e5b2418361b9f575843
Rorisang Putu <rorisangputu@gmail.com>
2025-06-24 11:40:32 +0200

    installing stripe and importing stripe

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,9 +1,19 @@
+import db from "@/db/db";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 export default async function PurchasePage({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
+  const product = await db.product.findUnique({
+    where: { id },
+  });
+
+  if (product == null) return notFound();
   return (
     <div>
       <h1>Hii {id}</h1>
