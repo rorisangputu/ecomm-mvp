9d1fe40e39b8480d2cc74b34d7abf2b4d4ebb96b
Rorisang Putu <rorisangputu@gmail.com>
2025-06-25 13:44:39 +0200

    changing users/customers link

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -14,7 +14,7 @@ export default function AdminLayout({
         <NavLink href={"/admin"}>Dashboard</NavLink>
         <NavLink href={"/admin/products"}>Products</NavLink>
         <NavLink href={"/admin/orders"}>Sales</NavLink>
-        <NavLink href={"/admin/customers"}>customers</NavLink>
+        <NavLink href={"/admin/users"}>Customers</NavLink>
       </Nav>
       <div className="w-[90%] xl:w-[70%] mx-auto my-6">{children}</div>
     </>
