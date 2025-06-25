0f69b158cf85de4db9551fd08df87a7c2b4a44f8
Rorisang Putu <rorisangputu@gmail.com>
2025-06-25 08:19:56 +0200

    post purchase success page

--- src/app/(customerFacing)/stripe/purchase-success/page.tsx
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -1,3 +1,59 @@
-export default function SuccessPage() {
-  return <h1>Success go home!!</h1>;
+import { Button } from "@/components/ui/button";
+import db from "@/db/db";
+import { formatCurrency } from "@/lib/formatter";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+export default async function SuccessPage({
+  searchParams,
+}: {
+  searchParams: Promise<{
+    payment_intent: string;
+  }>;
+}) {
+  const { payment_intent } = await searchParams;
+  const paymentIntent = await stripe.paymentIntents.retrieve(payment_intent);
+
+  if (paymentIntent.metadata.productId === null) return notFound();
+
+  const id = paymentIntent.metadata.productId;
+
+  const product = await db.product.findUnique({
+    where: { id: id },
+  });
+
+  if (product == null) return notFound();
+  const isSuccess = paymentIntent.status === "succeeded";
+
+  return (
+    <div className="max-w-5xl w-full mx-auto space-y-8">
+      <h1 className="text-4xl font-bold text-green-500">
+        {isSuccess ? "Success!" : "Error"}{" "}
+      </h1>
+      <div className="flex gap-4 items-center">
+        <div className="aspect-video flex-shrink-0 w-1/3 relative">
+          <Image
+            src={product.imagePath}
+            fill
+            alt={product.name}
+            className="object-cover"
+          />
+        </div>
+        <div>
+          <div className="text-lg">
+            {formatCurrency(product.priceInCents / 100)}
+          </div>
+          <h1 className="text-2xl font-bold">{product.name}</h1>
+          <div className="line-clamp-3 text-muted-foreground">
+            {product.description}
+          </div>
+          <Button className="mt-4" size="lg" asChild>
+            {isSuccess ? "Download File" : "Oops"}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
