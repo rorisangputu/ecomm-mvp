a44ef592ea9875f4552727ed392f991974b4fffe
Rorisang Putu <rorisangputu@gmail.com>
2025-06-25 09:08:58 +0200

    download file function

---
+++ src/app/(customerFacing)/products/download/[downloadVerificationId]/route.ts
@@ -0,0 +1,38 @@
+import db from "@/db/db";
+import { NextRequest, NextResponse } from "next/server";
+import fs from "fs/promises";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ downloadVerificationId: string }> }
+) {
+  const { downloadVerificationId } = await params;
+
+  const data = await db.downloadVerifaction.findUnique({
+    where: { id: downloadVerificationId, expiresAt: { gt: new Date() } },
+    select: {
+      product: {
+        select: { filePath: true, name: true },
+      },
+    },
+  });
+
+  if (data == null) {
+    return NextResponse.redirect(
+      new URL("/products/download/expired", req.url)
+    );
+  }
+
+  const { size } = await fs.stat(data.product.filePath);
+  const file = await fs.readFile(data.product.filePath);
+  const extension = data.product.filePath.split(".").pop();
+
+  return new NextResponse(file as unknown as ArrayBuffer, {
+    headers: {
+      "Content-Disposition": `attachment; filename="${data.product.name}.${extension}"`,
+      "Content-Length": size.toString(),
+    },
+  });
+
+  return new NextResponse("Hi");
+}
